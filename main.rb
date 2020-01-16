c6dfb496d85b634d6d861bbdcaf8bd9df4abaf00
Yefim Vedernikoff <yefim323@gmail.com>
2020-01-16 01:13:43 -0500

    Make it lambda-compatible

--- main.rb
+++ main.rb
@@ -1,32 +1,36 @@
 require 'algoliasearch'
 require 'httparty'
 
-client = Algolia.init(
-  application_id: ENV['APP_ID'],
-  api_key: ENV['API_KEY']
-)
+def handler(event:, context:)
+  client = Algolia.init(
+    application_id: ENV['APP_ID'],
+    api_key: ENV['API_KEY']
+  )
 
-index = client.init_index('Listing_production');
+  index = client.init_index('Listing_production');
 
-yesterday = Time.now.to_i - 60 * 60 * 24
+  yesterday = Time.now.to_i - 60 * 60 * 24
 
-results = index.search('', {
-  filters: "created_at_i >= #{yesterday} AND (strata:grailed OR strata:hype OR strata:basic OR strata:sartorial) AND (designers.id:28) AND (category:tops OR category:outerwear OR category:bottoms OR category:tailoring OR category:accessories OR category_path_root_size:footwear.12 OR category_path_root_size:footwear.13) AND price_i>=0 AND (marketplace: grailed)",
-  hitsPerPage: 50,
-  page: 0,
-})
+  results = index.search('', {
+    filters: "created_at_i >= #{yesterday} AND (strata:grailed OR strata:hype OR strata:basic OR strata:sartorial) AND (designers.id:28) AND (category:tops OR category:outerwear OR category:bottoms OR category:tailoring OR category:accessories OR category_path_root_size:footwear.12 OR category_path_root_size:footwear.13) AND price_i>=0 AND (marketplace: grailed)",
+    hitsPerPage: 50,
+    page: 0,
+  })
 
-email = results['hits'].map do |res|
-  photo = res['retina_cover_photo']['url']
+  email = results['hits'].map do |res|
+    photo = res['retina_cover_photo']['url']
 
-  %Q(<div><img src="#{photo}" /><p><a href="https://www.grailed.com/listings/#{res['id']}">#{res['title']}</a> for #{res['price_i']}</p></div>)
-end.join('')
+    %Q(<div><img src="#{photo}" /><p><a href="https://www.grailed.com/listings/#{res['id']}">#{res['title']}</a> for #{res['price_i']}</p></div>)
+  end.join('')
 
-HTTParty.post(ENV['IFTTT_URL'], {
-  body: {
-    value1: email
-  }.to_json,
-  headers: {
-    'Content-Type' => 'application/json'
-  },
-})
+  puts email
+
+  HTTParty.post(ENV['IFTTT_URL'], {
+    body: {
+      value1: email
+    }.to_json,
+    headers: {
+      'Content-Type' => 'application/json'
+    },
+  })
+end
